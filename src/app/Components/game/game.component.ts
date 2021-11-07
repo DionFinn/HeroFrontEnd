25a865e9c63d04058226f6e0e52aff25a8622147
DionFinn <103545669@student.swin.edu.au>
2021-11-07 14:56:12 +1100

    6 files modified

--- src/app/Components/game/game.component.ts
+++ src/app/Components/game/game.component.ts
@@ -13,8 +13,8 @@ import { ResultComponent } from '../result/result.component';
   styleUrls: ['./game.component.css']
 })
 export class GameComponent implements OnInit {
-  heroList: Hero [] = [{HeroID: 5, HeroName: "Dion", MinDice: 4, MaxDice: 6, Uses: 3}, 
-                       {HeroID: 5, HeroName: "Dion", MinDice: 4, MaxDice: 6, Uses: 3}];
+  heroList: Hero [] = [{HeroID: 5, HeroName: "Dion", MinDice: 1, MaxDice: 6, Uses: 3}, 
+                       {HeroID: 5, HeroName: "Dion", MinDice: 1, MaxDice: 6, Uses: 3}];
   villanList: Villan [] = [{VillanID: 2, VillanName: "Shano", AttackPoints: 5},
                            {VillanID: 3, VillanName: "NotBad", AttackPoints: 5}];
   resultList: Result [] = [{ GameTime: new Date, Winner: 'Villan Wins'}]
@@ -36,21 +36,54 @@ export class GameComponent implements OnInit {
   }
 
   RollBtn(): void{
-    
+    let attack = this.randomAttack();
+    if(attack > this.selectedVillan.AttackPoints){
+      this.selectedVillan.AttackPoints = 0;
+    }
+    else {
+      this.selectedVillan.AttackPoints -= attack;
+    }
+   
+    this.selectedHero.Uses -= 1;
+    this.selectedHero = null;
+    this.selectedVillan = null;
   }
 
 
+
+
   onSelectHero(hero: Hero){
-    this.selectedHero = hero; 
+    if(this.selectedHero === hero){
+     this.selectedHero = null;
+    }
+    else {
+      if(hero.Uses > 0){
+      this.selectedHero = hero;
+      }
+    }
   }
 
 
 
   onSelectVillan(villan: Villan){
-    this.selectedVillan = villan
+    
+    if(this.selectedVillan === villan){
+      this.selectedVillan = null;
+     }
+     else {
+       if(villan.AttackPoints > 0){
+      this.selectedVillan = villan;
+      }
+    }
   }
 
+  randomAttack(): number {
+    const roll = Math.floor(Math.random() * (this.selectedHero.MaxDice - this.selectedHero.MinDice + 1)) + this.selectedHero.MinDice;
+    console.log(roll);
+    return roll;
+  }
 }
+
 
 /*Hero section
 disabled when users = 0
@@ -59,5 +92,3 @@ selects highlights
 disables other selected (only select when uses is greater than 0)
 when Uses zero disabled
 */
-
-
--- src/app/Models/Hero.ts
+++ src/app/Models/Hero.ts
@@ -4,4 +4,11 @@ export class Hero {
   MinDice: number;
   MaxDice: number
   Uses: number;
+  constructor( HeroName : string ){
+    this.HeroID = this.HeroID;
+    this.HeroName = this.HeroName;
+    this.MinDice = this.MinDice
+    this.MaxDice = this.MaxDice
+    this.Uses = this.Uses;
+  }
 }
