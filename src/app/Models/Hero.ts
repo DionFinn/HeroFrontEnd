0d0aaf7d81d2af3c14e7d33264575565bbc9401c
DionFinn <103545669@student.swin.edu.au>
2021-11-09 16:06:38 +1100

    2 files modified

--- src/app/Models/Hero.ts
+++ src/app/Models/Hero.ts
@@ -1,14 +1,25 @@
+export interface HeroImport {
+  id: number;
+  name: string;
+  minRoll: number;
+  maxRoll: number;
+  startingUses: number;
+}
+
+
 export class Hero {
   HeroID: number;
   HeroName: string;
   MinDice: number;
   MaxDice: number
   Uses: number;
-  constructor( HeroName : string ){
+  constructor( HeroImport : HeroImport ){
     this.HeroID = this.HeroID;
     this.HeroName = this.HeroName;
     this.MinDice = this.MinDice
     this.MaxDice = this.MaxDice
     this.Uses = this.Uses;
   }
+
+
 }
--- src/app/Services/hero.service.ts
+++ src/app/Services/hero.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Hero, HeroImport } from '.././Models/hero';
+import { map } from 'rxjs/operators';
+import { BaseService } from './base.service';
 
-@Injectable()
-export class HeroService {
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService extends BaseService {
 
-  constructor() { }
+  constructor(private _http: HttpClient) {
+    super();
+  }
 
+  getAllHeroes(): Observable<Hero[]> {
+    return this._http.get<HeroImport[]>(this.baseUrl + "Hero").pipe(map(heroDataArray => heroDataArray.map(HeroImport => new Hero(HeroImport))));
+  }
 }
