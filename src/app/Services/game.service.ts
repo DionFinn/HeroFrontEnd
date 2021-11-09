c8be85a507348ace789edb4f21c8136178d2adcb
DionFinn <103545669@student.swin.edu.au>
2021-11-09 15:59:16 +1100

    1 file modified

--- src/app/Services/game.service.ts
+++ src/app/Services/game.service.ts
@@ -1,8 +1,25 @@
+
+   
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Result } from './../Models/Result';
+import { BaseService } from './base.service';
 
-@Injectable()
-export class GameService {
 
-  constructor() { }
 
+@Injectable({
+  providedIn: 'root'
+})
+export class GameService extends BaseService {
+
+  constructor(private _http: HttpClient) {
+    super();
+  }
+  getAllGames(): Observable<Result[]> {
+    return this._http.get<Result[]>(this.baseUrl + "Game");
+  }
+  addNewGame(newGame: Result): Observable<Result> {
+    return this._http.post<Result>(this.baseUrl + "Game", newGame);
+  }
 }
